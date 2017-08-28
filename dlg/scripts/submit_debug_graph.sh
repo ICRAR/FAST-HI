3630c798b4ab896408e577bacc8ff8d699eb2ec3
Rodrigo Tobar <rtobar@icrar.org>
2017-08-28 13:54:12 +0800

    Removing unnecessary -p option
    The -p option also needs a value, so the previous version was hanging while
    waiting for contents coming from the stdin.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dlg/scripts/submit_debug_graph.sh
+++ dlg/scripts/submit_debug_graph.sh
@@ -8,4 +8,4 @@ lg_dir="$this_dir"/../logical_graphs
 lg_file="$lg_dir"/debug/cal_nongas.json
 
 # Translate into a physical graph template, partition, etc, and finally submit
-dlg unroll-and-partition -p -L "$lg_file" | dlg map | dlg submit
+dlg unroll-and-partition -L "$lg_file" | dlg map | dlg submit
