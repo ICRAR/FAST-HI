9702497295766182242c293448d6a891749a6e75
Rodrigo Tobar <rtobar@icrar.org>
2017-08-29 16:32:36 +0800

    Allow exposing managers through all ifaces
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dlg/scripts/start_procs.sh
+++ dlg/scripts/start_procs.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+function print_usage {
+	echo "$0 [-h | -?] [-A]"
+	echo
+	echo "-h, -?: Show this help"
+	echo "-A: Expose the manager processes through all network interfaces"
+}
+
+# Handle command-line arguments
+ALL_IFACES=no
+
+while getopts "Ah?" opt
+do
+	case "$opt" in
+		A)
+			ALL_IFACES=yes
+			;;
+		[h?])
+			print_usage
+			exit 0
+			;;
+		:)
+			print_usage 1>&2
+			exit 1
+	esac
+done
+
+iface_spec=""
+if [ "${ALL_IFACES}" = "yes" ]
+then
+	iface_spec="-H 0.0.0.0"
+fi
+
 # Where are we?
 this_dir="`dirname $0`"
 
@@ -10,10 +42,10 @@ cd "$this_dir"/../../modules
 # Start the Node Manager
 # -vv gives DEBUG output. More v is more verbose. Use "-q" for quiteness
 # -d starts the process as a daemon, can be stopped via -s later on
-dlg nm -vv -d
+dlg nm ${iface_spec} -vv -d
 
 # Start the Data Island Manager
 # -N localhost makes it point to our Node Manager running on localhost
 # -d works as above
 # Use -v as above if required
-dlg dim -N localhost -d
+dlg dim ${iface_spec} -N localhost -d
