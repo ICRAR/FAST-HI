20040e49788bdbf8ac7dcc283d2473264a3be070
Rodrigo Tobar <rtobar@icrar.org>
2017-08-29 16:30:37 +0800

    Correct verbosity specification
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dlg/scripts/start_procs.sh
+++ dlg/scripts/start_procs.sh
@@ -8,9 +8,9 @@ this_dir="`dirname $0`"
 cd "$this_dir"/../../modules
 
 # Start the Node Manager
-# -v 5 gives DEBUG output, lower values are less verbose
+# -vv gives DEBUG output. More v is more verbose. Use "-q" for quiteness
 # -d starts the process as a daemon, can be stopped via -s later on
-dlg nm -v 5 -d
+dlg nm -vv -d
 
 # Start the Data Island Manager
 # -N localhost makes it point to our Node Manager running on localhost
