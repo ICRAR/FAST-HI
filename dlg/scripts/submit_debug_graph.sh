ec11514b650105b6e133eda945eb1634b3bf7369
skitaeff <slava.kitaeff@icrar.org>
2017-09-04 15:20:49 +0800

    added -f and -o

--- dlg/scripts/submit_debug_graph.sh
+++ dlg/scripts/submit_debug_graph.sh
@@ -4,8 +4,10 @@ function print_usage {
 	echo "$0 [-h | -?] [-c <config_dir>] [-d <data_dir>]"
 	echo
 	echo "-h, -?: Show this help"
-	echo "-c <config_dir>: Indicate the directory containing all configuration files"
-	echo "-d <data_dir>: Indicate the directory containing all the data"
+	echo "-c <config_dir>: Provide a directory with all configuration files"
+	echo "-d <input_data_dir>: Provide a directory containing the observation"
+	echo "-f <observation>: Provide the observation file name"
+    echo "-o <data_dir>: Provide a directory for the output"
 }
 
 # Where are we?
@@ -28,6 +30,12 @@ do
 		d)
 			DATA_DIR="$OPTARG"
 			;;
+        f)
+            IN_FILE="$OPTARG"
+            ;;		
+        o)
+            OUT_DIR="$OPTARG"
+            ;;            	
 		[h?])
 			print_usage
 			exit 0
@@ -41,5 +49,5 @@ done
 # Replace the placeholder variables (i.e., transition from a Logical Graph
 # Template into a Logical Graph).
 # Then translate into a physical graph template, partition, etc, and finally submit
-sed "s|\${DATA_DIR}|${DATA_DIR}|g; s|\${CONFIG_DIR}|${CONFIG_DIR}|g" "$lg_file" \
+sed "s|\${DATA_DIR}|${DATA_DIR}|g; s|\${CONFIG_DIR}|${CONFIG_DIR}|g; s|\${IN_FILE}|${IN_FILE}|g; s|\${OUT_DIR}|${OUT_DIR}|g" "$lg_file" \
 	| dlg unroll-and-partition | dlg map | dlg submit
