e947fde07a0c182c6fa24e52b5de4eeb00205380
skitaeff <slava.kitaeff@icrar.org>
2017-09-04 15:26:48 +0800

    sed fixed

--- dlg/scripts/submit_debug_graph.sh
+++ dlg/scripts/submit_debug_graph.sh
@@ -46,5 +46,5 @@ done
 # Replace the placeholder variables (i.e., transition from a Logical Graph
 # Template into a Logical Graph).
 # Then translate into a physical graph template, partition, etc, and finally submit
-sed "s|\${DATA_DIR}|${DATA_DIR}|g; s|\${CONFIG_DIR}|${CONFIG_DIR}|g; s|\${IN_FILE}|${IN_FILE}|g; s|\${OUT_DIR}|${OUT_DIR}|g" "$lg_file" \
+sed "s|\${CONFIG_DIR}|${CONFIG_DIR}|g; s|\${IN_FILE}|${IN_FILE}|g; s|\${OUT_DIR}|${OUT_DIR}|g" "$lg_file" \
 	| dlg unroll-and-partition | dlg map | dlg submit
