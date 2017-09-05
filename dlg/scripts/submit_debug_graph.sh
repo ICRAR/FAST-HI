d248dcbf6835908e4a877867e3c71edfcfce2cbd
Rodrigo Tobar <rtobar@icrar.org>
2017-09-05 16:44:05 +0800

    OBS_NAME/DATA_DIR used to calculate filepaths
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dlg/scripts/submit_debug_graph.sh
+++ dlg/scripts/submit_debug_graph.sh
@@ -4,9 +4,9 @@ function print_usage {
 	echo "$0 [-h | -?] [-c <config_dir>] [-f <in_file>]  [-o <out_dir>]"
 	echo
 	echo "-h, -?: Show this help"
-	echo "-c <config_dir>: Provide a directory with all configuration files"
-	echo "-f <in_file>: Provide the observation file name"
-	echo "-o <out_dir>: Provide a directory for the output"
+	echo "-c <config_dir>:  Provide the directory with all configuration files"
+	echo "-d <data_dir>:    Provide the directory where all the data resides (or will reside)"
+	echo "-o <observation>: Provide the observation name (used to calculate filenames)"
 }
 
 # Where are we?
@@ -17,8 +17,8 @@ lg_dir="$this_dir"/../logical_graphs
 lg_file="$lg_dir"/debug/cal_nongas.json
 
 # Handle command-line arguments
-IN_FILE=input.ms
-OUT_DIR=.
+OBS_NAME=observation
+DATA_DIR=.
 CONFIG_DIR="$this_dir"/../../conf
 
 while getopts "c:f:o:h?" opt
@@ -27,11 +27,11 @@ do
 		c)
 			CONFIG_DIR="$OPTARG"
 			;;
-		f)
-			IN_FILE="$OPTARG"
+		d)
+			DATA_DIR="$OPTARG"
 			;;
 		o)
-			OUT_DIR="$OPTARG"
+			OBS_NAME="$OPTARG"
 			;;
 		[h?])
 			print_usage
@@ -46,5 +46,5 @@ done
 # Replace the placeholder variables (i.e., transition from a Logical Graph
 # Template into a Logical Graph).
 # Then translate into a physical graph template, partition, etc, and finally submit
-sed "s|\${CONFIG_DIR}|${CONFIG_DIR}|g; s|\${IN_FILE}|${IN_FILE}|g; s|\${OUT_DIR}|${OUT_DIR}|g" "$lg_file" \
+sed "s|\${CONFIG_DIR}|${CONFIG_DIR}|g; s|\${DATA_DIR}|${DATA_DIR}|g; s|\${OBS_NAME}|${OBS_NAME}|g" "$lg_file" \
 	| dlg unroll-and-partition | dlg map | dlg submit
