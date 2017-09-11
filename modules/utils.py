7bf937e34aaebaa88310341607bc0f611a9da115
Rodrigo Tobar <rtobar@icrar.org>
2017-09-11 16:37:24 +0800

    Centralizing command line argument checks
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,7 @@
 """Common utility routines used by the FAST pipeline modules"""
 
 import ConfigParser
+import os
 import sys
 
 # ConfigParser enhancements to get list of objects
@@ -36,3 +37,34 @@ def cmdline_cleanup():
             i = i + 2
             break
     return sys.argv[i:]
+
+def check_ioargs(args, casalog, infiles=False):
+
+    # infile and outfile are mandatory
+    # if "infiles" is True then the argument is called "infiles" and should be
+    # a comma-separated list of filenames
+    if infiles and not args.infiles:
+        casalog.post('Infiles must be provided. Use --infiles.', priority="SEVERE")
+        sys.exit(1)
+    elif not infiles and not args.infile:
+        casalog.post('Infile must be provided. Use --.', priority="SEVERE")
+        sys.exit(1)
+    if not args.outfile:
+        casalog.post('Outfile must to be provided. Use --outfile.', priority="SEVERE")
+        sys.exit(1)
+
+    # Input files should exist
+    try:
+        infiles = args.infiles
+    except AttributeError:
+        infiles = [args.infile]
+
+    for infile in infiles:
+        if not os.path.exists(infile):
+            casalog.post('Input data %s does not exist' % args.infile, priority="SEVERE")
+            sys.exit(1)
+
+    # Ensure the directory holding outfile exists
+    outpath = os.path.dirname(args.outfile)
+    if not os.path.isdir(outpath):
+        os.makedirs(outpath)
